56db26595dfbee2c06f126be4a096670fec2bb8e
Peter Heri <heripeter725@gmail.com>
2024-09-02 11:31:20 +0300

    Added a multiColor button
    users can now sketch with different random colors to make their sketch lively

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const containerGrid = document.querySelector('.container');
 const eraser = document.querySelector('#eraser');
-const sketchBtn = document.querySelector('#sketchBtn')
+const sketchBtn = document.querySelector('#sketchBtn');
+const colorBtn = document.querySelector('#multicolorBtn');
+const gridSize = document.querySelector('#gridSize');
+
+
+
+
+function getGridSize(){
+    let sides = prompt('What is your grid size? ')
+}
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for(let i = 0; i < 6; i++){
+        color +=  letters[Math.floor(Math.random() * 16)];
+
+    }
+    return color;
+}
+
+
+
+ 
 
 
 
@@ -8,6 +31,7 @@ const sketchBtn = document.querySelector('#sketchBtn')
 
 
 function createGrid(sides) {
+    
 
     for (let y = 0; y < sides ; y++){
         const subgrid = document.createElement('div');
@@ -23,8 +47,8 @@ function createGrid(sides) {
             subgrid2.setAttribute('style', 'border: transparent; flex : 1;');
             subgrid2.classList.add('innerGrid')
             subgrid.appendChild(subgrid2)
-           
-
+            
+            
             
             subgrid2.addEventListener('mouseover',()=> {
                 subgrid2.classList.add('colored')
@@ -36,40 +60,56 @@ function createGrid(sides) {
             function sketch(){
                 
                 subgrid2.addEventListener('mouseover', ()=>{
-                    subgrid2.classList.add('colored')
+                    subgrid2.classList.remove('coloredMultiple');
+                    subgrid2.classList.add('colored');
                 })
             }
-
+            
             sketchBtn.addEventListener('click', ()=> {
+               
                 sketch();
-            })
-
+                console.log('clicked');
+            }, )
+            
+            
             function eraseSketch() {
                 
-            subgrid2.addEventListener('mouseover',()=> {
-                subgrid2.classList.remove('colored')
-                
-            })
+                subgrid2.addEventListener('mouseover',()=> {
+
+                    subgrid2.classList.remove('colored');
+                    subgrid2.removeAttribute('background-color');
+                    subgrid2.style.background = '';
+                    
+                })
             } 
             
                 eraser.addEventListener('click', ()=>{
                     eraseSketch()
+                    
                 })
-        
+
+
+                colorBtn.addEventListener('click', ()=>{
+                    subgrid2.addEventListener('mouseover', ()=>{
+                        subgrid2.classList.add('colored');
+                        subgrid2.style.background = getRandomColor();
+                        
+                    })
+                })
+            
+            
+        }
             
         }
         
-    
-}
+        
+        
+    }
 
     
     
-}
-
-
-
-createGrid(10);
-
-
+    createGrid(10);
+    
+    
 
 
